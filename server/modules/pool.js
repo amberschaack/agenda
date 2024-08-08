161d97ddca29ed02d324d7b8fde4bdda879cd90a
Amber Schaack <aschaack9@gmail.com>
2024-08-08 11:52:05 -0500

    deployment complete

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,10 +1,3 @@
-/* the only line you likely need to change is
-
- database: 'prime_app',
-
- change `prime_app` to the name of your database, and you should be all set!
-*/
-
 const pg = require('pg');
 let pool;
 
@@ -31,5 +24,8 @@ else {
         database: 'agenda',
     });
 }
+
+pool.on('connect', () => console.log(`Connected to database`));
+pool.on('error', (err) => console.error(`Error connecting to database:`, err));
 
 module.exports = pool;
